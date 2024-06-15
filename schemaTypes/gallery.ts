cff351cb5e4c979945e2937fe21122ed014571b8
Jeremy Hill <jhill83@gmail.com>
2024-06-14 22:51:48 -0500

    add gallery type

---
+++ schemaTypes/gallery.ts
@@ -0,0 +1,91 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'gallery',
+  title: 'Gallery',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [{
+        name: 'image',
+        type: 'image',
+        title: 'Image',
+        options: {
+          hotspot: true,
+        },
+        fields: [
+          {
+            name: 'alt',
+            type: 'string',
+            title: 'Alt',
+          },
+          {
+            name: 'caption',
+            type: 'string',
+            title: 'Caption',
+          },
+        ]
+      }]
+    }),
+    defineField({
+      name: 'category',
+      title: 'Category',
+      type: 'reference',
+      to: [{type: 'category'}],
+      options: {
+        filter: 'topLevel == true',
+      },
+    }),
+    defineField({
+      name: 'subCategories',
+      title: 'Sub-Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'blockContent',
+    }),
+    defineField({
+      name: 'blurb',
+      title: 'Blurb',
+      type: 'blockContent',
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      media: 'mainImage',
+      blurb: 'blurb',
+      slug: 'slug',
+    },
+    prepare(selection) {
+      const { blurb } = selection
+      return {...selection, subtitle: blurb && `${ blurb }`}
+    },
+  },
+})
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,6 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import page from './page'
+import gallery from './gallery'
 
-export const schemaTypes = [post, category, blockContent, page]
+export const schemaTypes = [post, category, blockContent, page, gallery,]
